5c24de41b997cce0ff117bf3147b0e540c04fb18
Sarah Basinger <sarrey@gmail.com>
2017-01-06 08:55:15 -0500

    clean up js

--- cities.js
+++ cities.js
@@ -64,4 +64,30 @@ cities.push(new City ("39","Atlanta","Georgia","456,002","420,003","+8.57%","133
 
 
 
-// console.log(cities);
+
+var buttonHTML = "";
+buildButtons();
+var map; 
+
+function buildButtons(){
+  for(let i = 0; i < cities.length; i++){
+    buttonHTML += '<button class="btn btn-default" id="'+[i]+'" onclick="addMarker('+[i]+')">'+cities[i].city+'</button>';
+  }
+  document.getElementById('city-buttons').innerHTML = buttonHTML;
+}
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: 39.0286, lng: -96.8314}
+  });
+}
+
+function addMarker(num){
+  console.log(num)
+  var marker = new google.maps.Marker({
+  position: {lat: cities[num].lat, lng: cities[num].lon},
+  map: map
+  });
+}
+
