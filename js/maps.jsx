7957716de8acc86a142c45136245e06ef349ebb5
Sarah Basinger <sarrey@gmail.com>
2017-01-11 14:11:10 -0500

    google maps marker and directions app

--- js/maps.jsx
+++ js/maps.jsx
@@ -7,11 +7,47 @@ var map = new google.maps.Map(
 	}
 );
 
+
 var infoWindow = new google.maps.InfoWindow({});
 var markers = [];
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var mapOptions = {
+	center: {lat: 39.8282, lng: -98.5795},
+	zoom: 4
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+
+}
+
+var start = 'Atlanta, GA';
+var end;
+function calcRoute() {
+  var request = 
+  	{
+		origin: start,
+		destination: end,
+		travelMode: 'DRIVING'
+	};
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
+
+initialize();
+
+
+
 function createMarker(city){
-	var icon = 'http://i.imgur.com/eQ3pSuK.png'
+	var icon = 'http://i.imgur.com/RJ969om.png'
 	var cityLL = {
 		lat: city.lat,
 		lng: city.lon
@@ -23,7 +59,7 @@ function createMarker(city){
 		icon: icon
 	})
 	google.maps.event.addListener(marker,'click', function(event){
-		infoWindow.setContent(`<h2>${city.city}</h2><div>${city.state}</div>`);
+		infoWindow.setContent(`<div><strong>${city.city}</strong></div><div>${city.state}</div><div><p onclick= class="zoom-text">Click to zoom</p>`);
 		infoWindow.open(map,marker);
 	});
 	markers.push(marker);
@@ -35,11 +71,16 @@ var GoogleCity = React.createClass({
 		console.log("someone clicked a city");
 		google.maps.event.trigger(markers[this.props.cityObject.yearRank-1],"click")
 	},
+	getDirections: function(){
+		end = this.props.cityObject.city;
+		calcRoute();
+	},
 	render: function(){
 		return(
 			<tr>
 				<td className="city-name" onClick={this.handleClickedCity}>{this.props.cityObject.city}</td>
 				<td className="city-rank">{this.props.cityObject.yearRank}</td>
+				<td><button className="btn btn-default btn-sm" onClick={this.getDirections}>Get directions</button></td>
 			</tr>
 		)	
 	}
@@ -73,7 +114,16 @@ var Cities = React.createClass({
 			createMarker(city)
 		});
 	},
+	setStartingLocation: function(event){
+		start = event.target.value;
+		
+	},
+	setStartingLocationText: function(event){
 
+		this.setState({
+			start: start
+		})
+	},
 	render: function(){
 		var cityRows = [];
 		this.state.currCities.map(function(currentCity, index){
@@ -82,21 +132,33 @@ var Cities = React.createClass({
 		});
 		return(
 			<div>
+				<div className="form-group form-inline">
+					<form onSubmit={this.updateMarkers}>
+						<input className="input-items" placeholder="Search for city" type="text" onChange={this.handleInputChange} />
+						<input className="btn btn-default btn-sm top-buttons" type="submit" value="Update markers on map"/>
+					</form>
+				</div>
 				<form onSubmit={this.updateMarkers}>
-					<input type="text" onChange={this.handleInputChange} />
-					<input type="submit" value="update markers"/>
+					<input className="input-items" placeholder="Atlanta, GA" type="text" onChange={this.setStartingLocation} />
+					<input className="btn btn-default btn-sm top-buttons" type="submit" onClick={this.setStartingLocationText} value="Set starting point"/>
 				</form>
-				<table>
-					<thead>
-						<tr>
-							<th>City Name</th>
-							<th>City Rank</th>
-						</tr>
-					</thead>
-					<tbody>
-						{cityRows}
-					</tbody>
-				</table>
+				<div className=" starting-location">
+					<strong>Starting location:</strong> {start}
+				</div>
+				<div className="col-xs-10">
+					<table className="table table-striped">
+						<thead>
+							<tr>
+								<th>City Name</th>
+								<th>City Rank</th>
+								<th>&nbsp;</th>
+							</tr>
+						</thead>
+						<tbody>
+							{cityRows}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		)
 	}
